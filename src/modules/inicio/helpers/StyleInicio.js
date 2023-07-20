e110b7090ebd9807e082a965dfa205b542bceb29
Héctor Benjamín Chipagua Calles <hector7chess@gmail.com>
2023-07-20 11:48:44 -0600

    Maquetado de pagina de inicio listo / pendiente animaciones

---
+++ src/modules/inicio/helpers/StyleInicio.js
@@ -0,0 +1,51 @@
+const blanco = 'solid 4px  rgba(255, 255, 255, 0.7'
+const widthMarco = window.screen.width > 900 ? '30%' : '45%'
+const heightMarco = window.screen.width > 900 ? '50%' : '20%'
+const rightMarco = window.screen.width > 900 ? '-70%' : '-55%'
+const topMarco = window.screen.width > 900 ? '50%' : '80%'
+const degradadoPurple = 'linear-gradient(90deg, rgba(150,101,253,0.6) 0%, rgba(150,101,253,1) 50%, rgba(150,101,253,0.6) 100%)'
+
+const viewInicio = { width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden', backgroundColor: '#322AA4' }
+
+// Titulos
+const contentTitles = { width: '90vw', height: '80vh', position: 'fixed', zIndex: 3, top: '7em', }
+const dotI = { borderRadius: '50%', background: degradadoPurple, width: '1.5em', height: '1.5em', margin: '0px 0.3em'}
+const subtitleMain = { margin: 0, fontFamily: "'Roboto', sans-serif", fontWeight: 400, fontSize: '1.2em', lineHeight: '1.2em', color: '#fff' }
+const titleMain = { margin: 0, fontFamily: "'Roboto', sans-serif", fontWeight: 700, fontSize: '7em', lineHeight: '0.8em', color: '#fff' }
+const btnDesk = {
+    textDecoration: 'none', background: degradadoPurple, color: '#fff', borderRadius: '3px',
+    padding: '0.5em 1em', fontWeight: 700, border: "3px solid rgba(0,0,0,0.1)",
+    boxShadow: '0px 0px 3px 2px rgba(255,255,255,0.6)', position: 'relative', bottom: '-1em', marginLeft: '0.3em'
+  }
+const btnMobil = { ...btnDesk , position: 'absolute', bottom: '5em', marginLeft: '5%', textAlign:'center', fontSize: '1.5em', width: '90%'}
+const logoDemente = { width: '80%', position: 'absolute', bottom: '3em', zIndex: 2, margin: '0px 10%', opacity: 0.8 }
+const logoCreativa = { width: '70%', position: 'absolute', bottom: '2em', zIndex: 2, margin: '0px 15%' }
+
+// Seccion de camara
+const contentCamera = {
+    width: '90vw', height: window.screen.width > 900 ? '80vh' : '85vh', position: 'fixed', zIndex: 2, top: '6.2em', display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)',
+    gridTemplateRows: 'repeat(2, 1fr)'
+  }
+  const cornerTopLeft = { width: widthMarco, height: heightMarco, borderTopLeftRadius: '10px', borderLeft: blanco, borderTop: blanco }
+  const cornerTopRight = { width: widthMarco, height: heightMarco, borderTopRightRadius: '10px', borderRight: blanco, borderTop: blanco, position: 'relative', right: rightMarco }
+  const cornerBottomLeft = { width: widthMarco, height: heightMarco, borderBottomLeftRadius: '10px', borderLeft: blanco, borderBottom: blanco, position: 'relative', top: topMarco }
+  const cornerBottomRight = { width: widthMarco, height: heightMarco, borderBottomRightRadius: '10px', borderRight: blanco, borderBottom: blanco, position: 'relative', right: rightMarco, top: topMarco }
+
+  const video = { width: window.screen.width > 900 ? '100vw' : 'auto', height: window.screen.width > 900 ? 'auto' : '100vh', maxWidth: '1920px', position: 'fixed', zIndex: 1 }
+export const stylesInicio = {
+    viewInicio,
+    contentTitles,
+    dotI,
+    subtitleMain,
+    titleMain,
+    btnDesk,
+    btnMobil,
+    logoDemente,
+    logoCreativa,
+    contentCamera,
+    cornerTopLeft,
+    cornerTopRight,
+    cornerBottomLeft,
+    cornerBottomRight,
+    video
+}
--- src/modules/inicio/pages/InicioPage.jsx
+++ src/modules/inicio/pages/InicioPage.jsx
@@ -1,27 +1,48 @@
 import { AppLayout } from "../../../layouts/AppLayout"
-import  videoSea  from '../assets/bg.mp4'
+import videoSea from '../assets/bg.mp4'
 import creativaLogo from "../assets/creativa-agency.svg";
+import DEMENTElogo from "../assets/DEMENTE.svg";
+import { stylesInicio } from "../helpers/StyleInicio"
+
+import { motion } from "framer-motion"
+
 
 export const InicioPage = () => {
+  
   return (
     <AppLayout>
-    <div className="InicioPage" style={{width:'100vw', height:'100vh', display:'flex', justifyContent:'center',alignItems:'center',overflow:'hidden', backgroundColor:'#322AA4'}}>
-      <div className="contenTitles" style={{width:'90vw', height:'80vh', position:'fixed', zIndex:3, top:'7em',}}>
-          <div style={{width:'100%', height:'50%', position:'relative',bottom:'-50%'}}>
-              <p style={{margin:0,fontSize:'18em',lineHeight:'1em', fontFamily:"'Roboto', sans-serif", fontWeight:'bold', WebkitTextStroke:'2px rgba(255, 255, 255, 0.8)', color:'#fff0', 
-              position:'absolute',bottom:'0.05em',zIndex:1, width:'100%',textAlign:'center'}}>DEMENTE</p>
-              <img src={creativaLogo} alt="creativa agency" style={{width:'70%', position:'absolute',bottom:'2em',zIndex:2,margin:'0px 15%'}}/>
+      <div className="InicioPage" style={stylesInicio.viewInicio}>
+        {/* Titulos */}
+        <div style={stylesInicio.contentTitles}>
+          <div style={{ width: '100%', height: '50%', position: 'relative', padding: '1em' }}>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <motion.div style={stylesInicio.dotI} initial={{opacity:0}} animate={{ opacity:1 }} transition={{ repeat: Infinity, duration: 1 }} />
+              <p className="subtTitle1" style={stylesInicio.subtitleMain}>Más allá de las buenas</p>
+            </div>
+            <p style={stylesInicio.titleMain}>IDEAS</p>
+            <p style={{ ...stylesInicio.subtitleMain, marginLeft: '0.3em'}}>Marketing fuera de este mundo</p>
+            {
+              window.screen.width > 900 && <a href="/" style={stylesInicio.btnDesk}>VER SERVICIOS  DMNT</a>
+            }
           </div>
+          <div style={{ width: '100%', height: '50%', position: 'relative' }}>
+            {
+              window.screen.width < 900 && <a href="/" style={stylesInicio.btnMobil}>VER SERVICIOS  DMNT</a>
+            }
+            <img src={DEMENTElogo} alt="DEMENTE" style={stylesInicio.logoDemente} />
+            <img src={creativaLogo} alt="creativa agency" style={stylesInicio.logoCreativa} />
+          </div>
+        </div>
+        {/* Marco de camara */}
+        <div style={stylesInicio.contentCamera}>
+          <div style={stylesInicio.cornerTopLeft}></div>
+          <div style={stylesInicio.cornerTopRight}></div>
+          <div style={stylesInicio.cornerBottomLeft}></div>
+          <div style={stylesInicio.cornerBottomRight}></div>
+        </div>
+        {/* Video */}
+        <video style={stylesInicio.video} src={videoSea} autoPlay muted loop></video>
       </div>
-      <div style={{width:'90vw', height:'80vh', position:'fixed', zIndex:2, top:'7em', display:'grid', gridTemplateColumns:'repeat(2, 1fr)',
-    gridTemplateRows:'repeat(2, 1fr)'}}>
-        <div style={{width:'30%', height:'50%', borderTopLeftRadius:'30px', borderLeft:'solid 4px #fff', borderTop:'solid 4px #fff'}}></div>
-        <div style={{width:'30%', height:'50%', borderTopRightRadius:'30px', borderRight:'solid 4px #fff', borderTop:'solid 4px #fff',position:'relative',right:'-70%'}}></div>
-        <div style={{width:'30%', height:'50%', borderBottomLeftRadius:'30px', borderLeft:'solid 4px #fff', borderBottom:'solid 4px #fff',position:'relative',top:'50%'}}></div>
-        <div style={{width:'30%', height:'50%', borderBottomRightRadius:'30px', borderRight:'solid 4px #fff', borderBottom:'solid 4px #fff',position:'relative',right:'-70%',top:'50%'}}></div>
-      </div>
-      <video style={{width:'100vw', maxWidth:'1920px', position:'fixed', zIndex:1}} src={videoSea} autoPlay muted loop></video>
-    </div>
     </AppLayout>
   )
 }
