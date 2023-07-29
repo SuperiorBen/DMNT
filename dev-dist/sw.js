dce515c67ef5abc50574bd33c9dbecbfeae667d8
Héctor Benjamín Chipagua Calles <31744517+SuperiorBen@users.noreply.github.com>
2023-07-29 00:00:58 -0600

    Corregir indicador de scroll

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gs055et352o"
+    "revision": "0.n21lrsoifj"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/modules/quieneSomos/components/background/BackgroundAnimated.jsx
+++ src/modules/quieneSomos/components/background/BackgroundAnimated.jsx
@@ -1,12 +1,14 @@
 import bgCreativa from "../../assets/background/bgTeamWork.png"
 import bgTwist from "../../assets/background/psycoTwister.svg"
 import bgStars from "../../assets/stars-bg.webp"
+import scrolldots from "../../assets/background/animations/scrolldot.json"
 
-import { motion } from "framer-motion"
+import { motion, useMotionValueEvent, useScroll } from "framer-motion"
+import Lottie from "lottie-react";
 import { useEffect, useRef, useState } from "react"
 import '../../assets/css/styleQuienesSomos.css'
 
-// Time text control 
+// Number of lines text control 
 const controlTime = window.screen.width > 900 ? 6 : 1
 let textTimes = controlTime
 let textBg = []
@@ -18,31 +20,31 @@ while (textTimes >= 0) {
 
 // css variables
 const xTwister = window.screen.width > 900 ? `calc(100vw - 29em)` : `calc(100vw - (27em - 14em))`
-const sizeAniText = window.screen.width > 900 ? (window.screen.width > 1536 ? '9em' : '5em') : '5em'
+const sizeAniText = window.screen.width > 900 ? (window.screen.width > 1536 ? '8em' : '5em') : '5em'
 
 export const BackgroundAnimated = () => {
 
-    const [isOpen
-        , setOpen
-    ] = useState(false)
+    const { scrollYProgress } = useScroll()
+
+    // Twister background control 
+    const [isOpen, setOpen] = useState(false)
+    // Text background animate control
     const [textAnimate, setTextAnimate] = useState(0)
-    
+    // content letter animate
     const ref = useRef(null);
+
+    // Letter animated funtions ===========================================================
 
     useEffect(() => {
         ref.current.children[textAnimate].style.color = 'rgb(150 101 253 / 80%)'
     }, [textAnimate])
 
-
-
     useEffect(() => {
         const timer = setTimeout(() => {
-          setOpen(!isOpen)
+            isOpen === false && setOpen(!isOpen)
         }, 1500);
         return () => clearTimeout(timer);
-      }, []);
-
-
+    }, []);
 
     const runTextAnimate = () => {
         ref.current.children[textAnimate].style.color = 'transparent'
@@ -53,12 +55,19 @@ export const BackgroundAnimated = () => {
         }
     }
     setTimeout(runTextAnimate, 500);
+    // END Letter animated funtions ===========================================================
+
+
+    useMotionValueEvent(scrollYProgress, "change", (latest) => {
+        console.log(latest)
+    })
 
     return (
-        <div className="background" style={{ Height: '100vh', width: '100vw', position: 'fixed', top: 0, left: 0, zIndex: 0 }}>
+        <div className="background" style={{ height: '100vh', width: '100vw', position: 'fixed', top: 0, left: 0, zIndex: 0 }}>
+
             <img src={bgCreativa} alt="bgCreativa" style={{ height: '100vh', width: window.screen.width > 900 ? '29em' : '27em', position: 'absolute', top: 0, left: window.screen.width > 900 ? "0em" : "-14em", zIndex: 1, mixBlendMode: 'multiply' }} />
 
-            <motion.div  layout transition={{ duration: 0.7 }}
+            <motion.div layout transition={{ duration: 0.7 }}
                 style={{
                     height: '100vh', width: xTwister,
                     position: 'absolute', top: 0, right: isOpen ? `calc(-1 * ${xTwister})` : 0,
@@ -71,15 +80,18 @@ export const BackgroundAnimated = () => {
 
             <div ref={ref} className="staggertext" style={{
                 height: '100vh', width: xTwister, position: 'absolute', top: 0, right: 0,
-                zIndex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', overflow: 'hidden',
+                zIndex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column-reverse', overflow: 'hidden',
                 writingMode: window.screen.width > 900 ? 'horizontal-tb' : 'vertical-lr', textOrientation: 'mixed'
             }}>
+
                 {
                     textBg.map((e, i) => <p key={i} style={{
                         margin: '0px', ontFamily: "'Roboto', sans-serif", fontWeight: 700, fontSize: sizeAniText,
-                        lineHeight: '1em', WebkitTextStroke: 'rgb(150 101 253 / 60%) 2px', color: 'transparent', userSelect: 'none'
-                    }}>{e.title} </p>)
+                        lineHeight: '1em', WebkitTextStroke: 'rgb(150 101 253 / 60%) 2px', color: 'transparent', 
+                        userSelect: 'none'
+                    }}>{e.title}</p>)
                 }
+                <Lottie style={{ position: 'absolute', left: window.screen.width > 900  ? '1em' : '0em', bottom: 0, height: '70vh' }} animationData={scrolldots} />
             </div>
 
             <img style={{ height: '100vh', width: '100vw', position: 'absolute', top: 0, left: 0, zIndex: 0, opacity: 0.2 }} src={bgStars} alt="Fondo estrellas" />
