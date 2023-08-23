ea50076f0822765dbf6c49a61c638bb6a89ddaf0
Héctor Benjamín Chipagua Calles <31744517+SuperiorBen@users.noreply.github.com>
2023-08-22 19:20:48 -0600

    404 pagina

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -7,6 +7,7 @@ const QuienesSomosPage = lazy( ()=> import("../modules/quieneSomos/pages/Quienes
 const ServicioPage = lazy( ()=> import("../modules/servicios/main/pages/ServicioPage") )
 const DetailPage = lazy( ()=> import("../modules/servicios/detail/pages/DetailPage") )
 const ContactoPage = lazy( ()=> import("../modules/contacto/pages/ContactoPage") )
+const Page404 = lazy( ()=> import("../layouts/Page404") )
 
 
 export const AppRouter = () => {
@@ -19,6 +20,7 @@ export const AppRouter = () => {
                 <Route path="/servicios" element={< ServicioPage />} />
                 <Route path="/servicios/:detail" element={< DetailPage />} />
                 <Route path="/contacto" element={< ContactoPage />} />
+                <Route path="/404" element={< Page404 />} />
             </Routes>
         </AnimatePresence>
     )
